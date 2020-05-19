a71675b48e8556780cb2c3ff15a4e57b383be95d
julio hernandez <hernandezjulio58@ymail.com>
2020-05-19 11:27:53 -0700

    added question set for second chapter, plan on finishing later today

--- ChapterTwo.js
+++ ChapterTwo.js
@@ -1,4 +1,6 @@
-// LinkedLists
+/*
+Maybe worth looking into C++, I hear that they have some data structures and methods already implemented.
+*/
 
 class LinkedListNode {
   constructor(data, next = null) {
@@ -11,4 +13,47 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
-}
+} /*
+    Question: Remove Duplicates. Write code to remove duplicates from an unsorted linked list. How would you solve
+    this problem if a temporary buffer is not allowed ?
+
+    
+*/ /*
+    Question: Return kth to last. Write a method to find the kth to last element of a singly linked list.
+
+    
+*/ /*
+    Question: Delete Middle Node. Implement an algorithm to delete a node in the middle (ie. Any node but the first 
+    and last node, not necessarily the exact middle node of a singly linked list, given only access to that 
+    node.
+
+    
+*/ /*
+    Question: Partition. Write code to partition a linked list arund a value x, such that all nodes less than x come
+     before all nodes greater than or equal to x. If x is contained within the list, the values of x only neded to
+    be after the elements less than x. The partiton element x can apperar anywhere om the "right partition"; it does 
+    not need to appear between the left and right partitions.
+
+    
+*/ /*
+    Question: Sum Lists. You have two numbers represented by a linked lists, where each node contains a single digit.
+     The digits are stored in reverse order, such that the 1's digit is at the head of the list. Write a function 
+     that adds the two numbers and returns the sum as a linked list.
+
+    
+*/ /*
+    Question: Palindromes. Implement a function to check if a linked list is a palindrome.
+
+    
+*/ /*
+    Question: Intersection. Given two (singly) linked lists, determine if the two lists intersect. Return the 
+    intersecting node. Note that the intersection is defined based on reference, not just value, That is, if the 
+    kth node of the first linked list is the exact same node (by reference) as the jth node of the second linked 
+    list then they are intersecting.
+
+    
+*/
+/*
+    Question: Loop Detection. Given a circular linked list, implement an algorithm that returns the node at the
+     beginning of the loop.
+*/
