d8037e53651edefda6df473dbf2d5e50ece7fb17
julio hernandez <hernandezjulio58@ymail.com>
2020-05-21 11:39:17 -0700

    1/2 text solution

--- ChapterTwo.js
+++ ChapterTwo.js
@@ -14,31 +14,36 @@ class LinkedList {
     this.head = null;
   }
 } /*
-    Question: Remove Duplicates. Write code to remove duplicates from an unsorted linked list. How would you solve
-    this problem if a temporary buffer is not allowed ?
+    Question: Remove Duplicates. Write code to remove duplicates from an unsorted linked list. 
+    How would you solve this problem if a temporary buffer is not allowed ?
 
-    
-*/ /*
-    Question: Return kth to last. Write a method to find the kth to last element of a singly linked list.
-
-    
+    Solution: Iterate through LinkedList with a hashmap that will check if that value has already
+    appeared or not. If it hasn't, then set that value to one on the hashmap and iterate to the next
+    value. If it has, then reassign the this.next pointer to point to the next node in the list to
+    delete the node.
 */ /*
     Question: Delete Middle Node. Implement an algorithm to delete a node in the middle (ie. Any node but the first 
     and last node, not necessarily the exact middle node of a singly linked list, given only access to that 
     node.
 
+    Solution: A really easy solution could be to just iterate three times and make sure that a 
+    linked list was at least 3 nodes long. If it is, then delete the 2nd node.s
+
     
 */ /*
-    Question: Partition. Write code to partition a linked list arund a value x, such that all nodes less than x come
-     before all nodes greater than or equal to x. If x is contained within the list, the values of x only neded to
-    be after the elements less than x. The partiton element x can apperar anywhere om the "right partition"; it does 
-    not need to appear between the left and right partitions.
+    Question: Partition. Write code to partition a linked list around a value x, such that all 
+    nodes less than x come before all nodes greater than or equal to x. If x is contained within 
+    the list, the values of x only neded to be after the elements less than x. The partiton 
+    element x can apperar anywhere om the "right partition"; it does not need to appear between 
+    the left and right partitions.
+
+    Solution: 
 
     
 */ /*
     Question: Sum Lists. You have two numbers represented by a linked lists, where each node contains a single digit.
-     The digits are stored in reverse order, such that the 1's digit is at the head of the list. Write a function 
-     that adds the two numbers and returns the sum as a linked list.
+    The digits are stored in reverse order, such that the 1's digit is at the head of the list. Write a function 
+    that adds the two numbers and returns the sum as a linked list.
 
     
 */ /*
@@ -53,7 +58,13 @@ class LinkedList {
 
     
 */
+
+/*
+    Question: Return kth to last. Write a method to find the kth to last element of a singly linked list.
+
+    
+*/
 /*
     Question: Loop Detection. Given a circular linked list, implement an algorithm that returns the node at the
-     beginning of the loop.
+    beginning of the loop.
 */
